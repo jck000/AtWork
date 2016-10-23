96c16ece23e4e29d9aff19e2fa270c88d83b12eb
nielsbekkers <Niels@MacBook-Pro-van-Niels-2.local>
2016-10-23 18:41:21 +0200

    update $toegelaten gedeelte

--- API/api.php
+++ API/api.php
@@ -1,9 +1,16 @@
 <?php
+/*
+ * @Author: Niels Bekkers
+ *
+ * @Description: API service that handle request-methods with data(JSON) and push it to mysql-database
+ *               Test it with httpRequester, add json data to 'content to send' and use POST/DELETE method
+ */
  
 // Haal de HTTP methode op, het urlpad en de request(JSON)
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 $input = json_decode(file_get_contents('php://input'),true);
+$toegelaten = null;
  
 // Verbind met de mysql database
 $link = mysqli_connect('localhost', 'root', 'root', 'testAPI');
@@ -30,38 +37,44 @@ for ($i=0;$i<count($columns);$i++) {
 // Maak de SQL-string naargelang de HTTP-methode
 switch ($method) {
   case 'GET':
-    $sql = "select * from `$table`".($key?" WHERE id=$key":''); break;
+    $toegelaten = "GET is niet toegelaten in deze service!"; break;
   case 'PUT':
-    $sql = "update `$table` set $set where id=$key"; break;
+    $toegelaten = "PUT is niet toegelaten in deze service!"; break;
   case 'POST':
-    /*$sql = "insert into `$table` set $set"; break;*/
-    $sql = "insert into categories (id) values ('5')"; break;
+    $sql = "insert into `$table` set $set"; break;
+    /*$sql = "insert into categories (id) values ('5')"; break;*/
   case 'DELETE':
-    /*$sql = "delete `$table` where id=$key"; break;*/
-    $sql = "delete from categories where id='5'"; break;
+    /*$sql = "delete `$table` where id=$set"; break;*/
+    $sql = "delete from categories where name='test'"; break;
 }
  
-// Voer SQL commando uit
-$result = mysqli_query($link,$sql);
+// Voer SQL commando uit indien toegelaten!
+if ($toegelaten == null){
+
+  $result = mysqli_query($link,$sql);
+
+  // Genereer foutmelding indien SQL commando foutief is
+  if (!$result) {
+    http_response_code(404);
+    die(mysqli_error());
+  }
  
-// Genereer foutmelding indien SQL commando foutief is
-if (!$result) {
-  http_response_code(404);
-  die(mysqli_error());
-}
- 
-// Toon resultaat in browser (visueel testen)
-if ($method == 'GET') {
-  if (!$key) echo '[';
-  for ($i=0;$i<mysqli_num_rows($result);$i++) {
-    echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
-  }}
+  // Toon resultaat in browser (visueel testen)
+  if ($method == 'GET') {
+    if (!$key) echo '[';
+      for ($i=0;$i<mysqli_num_rows($result);$i++) {
+      echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
+      }}
   /*if (!$key) echo ']';
-} elseif ($method == 'POST') {
-  echo mysqli_insert_id($link);
-} else {
-  echo mysqli_affected_rows($link);
-}*/
+    } elseif ($method == 'POST') {
+      echo mysqli_insert_id($link);
+    } else {
+      echo mysqli_affected_rows($link);
+    }*/
  
-// Sluit de SQL verbinding
-mysqli_close($link);
+  // Sluit de SQL verbinding
+  mysqli_close($link);
+}
+else{
+  echo $toegelaten;
+}
