c1d71a7f1f04ba68116c4dd559632eca86f3d6b2
nielsbekkers <Niels@MacBook-Pro-van-Niels-2.local>
2016-10-26 17:43:54 +0200

    homepage & settingspage basics (working)

--- AtWork/src/app/app.module.ts
+++ AtWork/src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import { Settings } from '../pages/settings/settings';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    Settings
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -20,10 +16,8 @@ import { TabsPage } from '../pages/tabs/tabs';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    Settings
   ],
   providers: []
 })
--- AtWork/src/pages/home/home.ts
+++ AtWork/src/pages/home/home.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { Settings } from '../settings/settings';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  settings = Settings;
 
   constructor(public navCtrl: NavController) {
 
